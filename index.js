4aaa75060d34bef64da578b178d4b4b7e7658b44
milindgowda <144997414+milindgowda@users.noreply.github.com>
2023-09-15 12:15:35 +0000

    new project init

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!")
+})
+
+app.get("/hello/:name", (req, res) => {
+    res.send("Hello, "+ req.params.name);
+})
+
+app.listen(3000, () => {
+    console.log("app on port 3000");
+})
